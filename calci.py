63148f10890d9fff9629f36989658dae79fc7ce5
Akshaya <dakshaya2k5@gmail.com>
2025-04-11 11:49:33 +0530

    Initial commit

---
+++ calci.py
@@ -0,0 +1,31 @@
+import math
+
+def get_input():
+    """Function to take input from the user."""
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    return a, b
+
+def add(a, b):
+    pass # Write your code here
+
+def subtract(a, b):
+    pass # Write your code here
+
+def multiply(a, b):
+    pass # Write your code here
+
+def divide(a, b):
+    pass # Write your code here
+
+def calculator():
+    print("\nWelcome to the Team Calculator!")
+    a, b = get_input()  # Taking input for `a` and `b`
+    print(f"\nValues received: a = {a}, b = {b}")
+
+    # Function - 1
+    #print(f"Addition: {add(a, b)}")
+
+
+if __name__ == "__main__":
+    calculator()
